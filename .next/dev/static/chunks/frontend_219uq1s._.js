ddbbf86db89ab70bf4a08d242e7055e34971fe1f
Bogdan Bogdanovic <bogdan.v.bogdanovic@gmail.com>
2026-08-08 23:39:51 +0200

    Remove .next build chunks from git tracking and configure .gitattributes for Solidity & TypeScript stats

--- .next/dev/static/chunks/frontend_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/frontend_src_app_globals_1cr0vsz.css","static/chunks/19xk_next_dist_0r0fjwb._.js","static/chunks/frontend_src_0980anx._.js","static/chunks/19xk_1f5hs2h._.js"],
-    source: "entry"
-});
